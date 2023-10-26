8deb8d94aedaa5ab763e877bd09ace37a45509eb
kwamekute <thatkutekwame@gmail.com>
2023-10-26 19:02:25 +0000

    keep scores and declare winner

--- rock.js
+++ rock.js
@@ -1,50 +1,75 @@
-let roundScore = 0
-let grandscore = 0
+let playerScore = 0;
+let compuScore = 0;
+
 
 
 let computerChoice = function () {
-    const options = ['rock', 'paper', 'scissors']
+    const options = ['rock', 'paper', 'scissors'];
 
     //select a random index from the options string above
-    const randomIndex = Math.floor(Math.random() * options.length)
+    const randomIndex = Math.floor(Math.random() * options.length);
 
     //use the the random index to select an element(rock or paper or scissors)
-    const randomChoice = options[randomIndex]
+    const randomChoice = options[randomIndex];
 
     return randomChoice;
 }
 
 let userChoice = function () {
-    return prompt("Enter your choice: ")
+    return prompt("Enter your choice: ");
 }
 
 const play = function () {
-    let userSelection = userChoice()
+    let userSelection = userChoice();
 
-    let computerSelection = computerChoice()
+    let computerSelection = computerChoice();
 
     if (userSelection === computerSelection) {
-        alert(`It's a tie you chose ${userSelection} computer chose ${computerSelection} `)
-        return roundScore += 1
+        alert(`It's a tie you chose ${userSelection} computer chose ${computerSelection} `);
+
+        return playerScore += 1;
     }
     else if (userSelection === "rock" && computerSelection === "paper") {
-        return alert(`You lose. You chose ${userSelection} computer chose ${computerSelection} `)
+
+        alert(`You lose. You chose ${userSelection} computer chose ${computerSelection} `);
+
+        return compuScore += 1;
+
     }
     else if (userSelection === "paper" && computerSelection === "scissors") {
-        return alert(`You lose. You chose ${userSelection} computer chose ${computerSelection} `)
+
+        alert(`You lose. You chose ${userSelection} computer chose ${computerSelection} `);
+
+        return compuScore += 1;
+
+
     }
     else if (userSelection === "scissors" && computerSelection === "rock") {
-        return alert(`You lose. You chose ${userSelection} computer chose ${computerSelection} `)
+        alert(`You lose. You chose ${userSelection} computer chose ${computerSelection} `);
+
+        return compuScore = + 1;
     }
 
 }
 
 play()
 
-console.log(roundScore)
+console.log(playerScore);
+console.log(compuScore);
 
 play()
-console.log(roundScore)
+console.log(playerScore);
+console.log(compuScore);
 
 play()
-console.log(roundScore)
+console.log(playerScore);
+console.log(compuScore);
+
+if (playerScore > compuScore) {
+
+    alert("You won! Refresh page to play again");
+}
+else {
+
+    alert("You lost! Refresh page to try again");
+}
